4a6bb3e135adc59b6dec6b5fa40d5d6cc780fa3a
GabrielRCodes <gabrielrcodes@gmail.com>
2022-11-14 17:18:44 -0300

    Adding OAuth props on _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
+function App({ Component, pageProps }: AppProps<{ session: Session; }>) {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
-};
+}
 
 export default App;
