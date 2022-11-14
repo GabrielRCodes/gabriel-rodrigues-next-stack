492c30927b8d907e9ec0eb4ae249d483c0260710
GabrielRCodes <gabrielrcodes@gmail.com>
2022-11-14 13:40:28 -0300

    Adding findMany

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// adicionando prisma.ts a pasta lib
+
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
